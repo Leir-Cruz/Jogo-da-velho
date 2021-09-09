232c3851b38a2e82574501cfbfbb6c18123a29e6
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2021-09-09 14:46:44 -0300

    adicionando teste para verificar ganhador diagonal

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -49,6 +49,25 @@ TEST_CASE("Ganhar pela linha", "[single-file]") {
     REQUIRE(VerificaVelha(teste3) == 1);
 }
 
+TEST_CASE("Ganhar pela diagonal", "[single-file]") {
+    int teste1[3][3]= { { 2, 1, 1 },
+                        { 0, 2, 0 },
+                        { 1, 0, 2 }
+                      };
+    REQUIRE(VerificaVelha(teste1) == 2);
+
+    int teste2[3][3]= { { 2, 2, 1 },
+                        { 0, 1, 0 },
+                        { 1, 2, 0 }
+                      };
+    REQUIRE(VerificaVelha(teste2) == 1);
+
+    int teste3[3][3]= { { 1, 0, 0 },
+                        { 2, 0, 2 },
+                        { 1, 1, 0 }
+                      };
+    REQUIRE(VerificaVelha(teste3) == 0);
+}
 
 
 
