36eb10a9d3430f4e6809c490d946d56445f7bc66
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2021-09-09 12:09:02 -0300

    vitoria por coluna preenchida

--- velha.cpp
+++ velha.cpp
@@ -13,9 +13,27 @@
  *  Descrever o que a funcao faz
  */ 
 
+int VerificaColuna( int matriz[3][3] )
+{
+	
+	for(int j = 0; j < 3; j++){
+	    if (matriz[0][j] == matriz[1][j] && matriz[1][j] == matriz[2][j]) {
+	    	return matriz[0][j];
+	    }
+	}
+	return false;
+
+}
+
 int VerificaVelha( int velha[3][3] )
 {
-	return 0; /*!< retorna zero para teste */
+	for(int j = 0; j < 3; j++) {
+		int ganhador = VerificaColuna(velha);
+		if(ganhador != 0) {
+			return ganhador;
+		}
+	}
+	return 0;
 }
 
 
