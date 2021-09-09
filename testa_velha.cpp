8d7e5603ea9f6975e229e8b8994ddd2458458057
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2021-09-09 13:58:57 -0300

    refatorando

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -22,12 +22,11 @@ TEST_CASE("Ganhar pela coluna", "[single-file]") {
                       };
     REQUIRE(VerificaVelha(teste2) == 2);
 
-	int teste3[3][3]= { { 2, 0, 1 },
+    int teste3[3][3]= { { 2, 0, 1 },
                         { 2, 0, 1 },
                         { 0, 0, 1 }
                       };
     REQUIRE(VerificaVelha(teste3) == 1);
-	
 }
 
 
